99f4dc36f1211dac7c2680c58168634c3ac2adda
kirstenmyette <kirstenmyette@aol.com>
2017-09-27 16:14:47 -0500

    changed/styled html more

--- java.js
+++ java.js
@@ -4,9 +4,9 @@ var friendsArray= ["Matt", "Josh", "January", "Brandi", "Daniel"]
 
 
 for (var i = 0; i < friends.length; i++) {
-  console.log(friends[i]);
-for (var j = 99 ; j > 1 ; j--) 
-    console.log(j + " lines of code in the file, " + j + " lines of code; " + friendsArray[i] + " strikes one out, clears it all out, " + (j-1) + " lines of code in the file");
+    console.log(friends[i]);
+    for (var j = 99; j > 1; j--)
+        console.log(j + " lines of code in the file, " + j + " lines of code; " + friendsArray[i] + " strikes one out, clears it all out, " + (j - 1) + " lines of code in the file");
     if (j = 1) {
         console.log(j + " line of code in the file, " + j + " line of code; " + friendsArray[i] + " strikes one out, clears it all out, no more lines of code in the file");
     }
