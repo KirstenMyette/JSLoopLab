773932aa4b7e1e2144b9d079e067886505c05f73
kirstenmyette <kirstenmyette@aol.com>
2017-09-27 10:31:47 -0500

    Completed Loop

---
+++ java.js
@@ -0,0 +1,13 @@
+var friends = ["Matt:", "Josh:", "January:", "Brandi:", "Daniel:"];
+var arrayLength = friends.length;
+
+
+
+for (var i = 0; i < arrayLength; i++) {
+  console.log(friends[i]);
+for (var j = 99 ; j > 1 ; j--) 
+    console.log(j + " lines of code in the file, " + j + " lines of code;" + i + " strikes one out, clears it all out," + i + " lines of code in the file");
+    if (j = 1) {
+        console.log(j + " line of code in the file, " + j + " line of code;" + i + " strikes one out, clears it all out," + i + " no more lines of code in the file");
+    }
+}
