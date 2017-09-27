e04071c1a3c1fcd99b30f9a0c71a3ae170a47ad1
kirstenmyette <kirstenmyette@aol.com>
2017-09-27 13:20:44 -0500

    99 lines of code complete

--- java.js
+++ java.js
@@ -1,13 +1,13 @@
 var friends = ["Matt:", "Josh:", "January:", "Brandi:", "Daniel:"];
-var arrayLength = friends.length;
+var friendsArray= ["Matt", "Josh", "January", "Brandi", "Daniel"]
 
 
 
-for (var i = 0; i < arrayLength; i++) {
+for (var i = 0; i < friends.length; i++) {
   console.log(friends[i]);
 for (var j = 99 ; j > 1 ; j--) 
-    console.log(j + " lines of code in the file, " + j + " lines of code;" + i + " strikes one out, clears it all out," + i + " lines of code in the file");
+    console.log(j + " lines of code in the file, " + j + " lines of code; " + friendsArray[i] + " strikes one out, clears it all out, " + (j-1) + " lines of code in the file");
     if (j = 1) {
-        console.log(j + " line of code in the file, " + j + " line of code;" + i + " strikes one out, clears it all out," + i + " no more lines of code in the file");
+        console.log(j + " line of code in the file, " + j + " line of code; " + friendsArray[i] + " strikes one out, clears it all out, no more lines of code in the file");
     }
 }
