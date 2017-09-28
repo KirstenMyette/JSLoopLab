3fbd79f96c897f380543bf749e85590dab125ba8
kirstenmyette <kirstenmyette@aol.com>
2017-09-28 10:51:55 -0500

    corrected grammar for when only 1 line of code left

--- java.js
+++ java.js
@@ -5,9 +5,20 @@ var friendsArray= ["Matt", "Josh", "January", "Brandi", "Daniel"]
 
 for (var i = 0; i < friends.length; i++) {
     console.log(friends[i]);
-    for (var j = 99; j > 1; j--)
-        console.log(j + " lines of code in the file, " + j + " lines of code; " + friendsArray[i] + " strikes one out, clears it all out, " + (j - 1) + " lines of code in the file");
-    if (j = 1) {
-        console.log(j + " line of code in the file, " + j + " line of code; " + friendsArray[i] + " strikes one out, clears it all out, no more lines of code in the file");
+
+    for (j = 99; j > 0; j--) { 
+
+
+    if (j === 2) {
+        console.log(j + " lines of code in the file, " + j + " lines of code; " + friendsArray[i] + " strikes one out, clears it all out, " + (j - 1) + " line of code in the file");
     }
+    else if (j === 1){
+        console.log(j + " line of code in the file, " + j + " line of code; " + friendsArray[i] + " strikes one out, clears it all out,  no more lines of code in the file");
+    }
+    else {
+        console.log(j + " line of code in the file, " + j + " line of code; " + friendsArray[i] + " strikes one out, clears it all out, " + (j - 1) + " lines of code in the file");
+    }
+    
+    }
+
 }
